c3d8f4aec9f74812272ba253340eb215f701c848
donnaj1 <142642580+donnaj1@users.noreply.github.com>
2023-08-31 13:32:38 -0400

    Add files via upload

---
+++ mainfunction.py
@@ -0,0 +1,11 @@
+#Main function
+def main():
+    value=int(17)
+    changevalue()
+    print('the value is', int(value))
+
+def changevalue():
+    value=int(50)
+    print ('the value is', int(value))
+
+main()
